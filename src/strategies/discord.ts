6bf1cd669aa85c6163a0a362d00c57ad3f67b96f
Alx010 <alex.d.dxtg@gmail.com>
2022-03-20 11:37:08 +0100

    added passport changes

---
+++ src/strategies/discord.ts
@@ -0,0 +1,13 @@
+import passport from "passport";
+import { Profile, Strategy } from "passport-discord";
+import { VerifyCallback } from "passport-oauth2";
+
+passport.use(new Strategy({
+    clientID: process.env.APP_ID!,
+    clientSecret: process.env.CLIENT_SECRET!,
+    callbackURL: process.env.REDIRECT_URL,
+    scope: ['identify', 'email', 'guilds'],
+}, async (accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) => {
+    
+})
+);
--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -3,13 +3,16 @@ import routes from '../routes';
 import cors from 'cors';
 import passport from "passport";
 import session from 'express-session';
+require('../strategies/discord');
 
 export function createApp(): Express {
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded());
     app.use(cors({ origin: ['https://localhost:3000'], credentials: true }));
-    app.use(session({secret: 'AKJKDBSDHASBDASDKASJDADJANSD', resave: false, saveUninitialized: false, cookie: { maxAge: 604800000 }}))
+    app.use(session({secret: 'AKJKDBSDHASBDASDKASJDADJANSD', resave: false, saveUninitialized: false, cookie: { maxAge: 604800000 }}));
+    app.use(passport.initialize());
+    app.use(passport.session());
     app.use('/api', routes);    
     return app;
 }
