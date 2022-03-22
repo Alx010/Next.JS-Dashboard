e455c7da8c2bc05744b821578c149b5ef8723ae1
Alx010 <alex.d.dxtg@gmail.com>
2022-03-22 15:12:00 +0100

    added db schemas

--- src/strategies/discord.ts
+++ src/strategies/discord.ts
@@ -1,13 +1,30 @@
 import passport from "passport";
 import { Profile, Strategy } from "passport-discord";
 import { VerifyCallback } from "passport-oauth2";
+import { User } from '../database/schemas';
 
 passport.use(new Strategy({
     clientID: process.env.APP_ID!,
     clientSecret: process.env.CLIENT_SECRET!,
     callbackURL: process.env.REDIRECT_URL,
-    scope: ['identify', 'email', 'guilds', 'connections'],
+    scope: ['identify', 'email', 'guilds'],
 }, async (accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) => {
     console.log(accessToken, refreshToken);
     console.log(profile);
+    const { id: discordId } = profile;
+    try {
+        const existingUser = await User.findOneAndUpdate(
+            { discordId },
+            { accessToken, refreshToken },
+            { new: true }
+        );
+        if (existingUser) return done(null, existingUser);
+
+        const newUser = new User({ discordId, accessToken, refreshToken});
+        const savedUser = await newUser.save();
+        return done(null, savedUser);
+    } catch(err) {
+        console.log(err);
+        return done(err as any, undefined)
+    }
 }));
